d6a53af0d691a2700033ffd28fa9c0cc6c4abedc
Puneet <puneetmungali93@gmail.com>
2018-06-30 13:15:24 +0530

    create task list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "../static/stylesheets/App.css";
+import Tasks from './Tasks';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        {/*<h1>Hello World!</h1>*/}
+        <Tasks/>
       </div>
     );
   }
---
+++ src/components/Tasks.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Tasks extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(<div>
+            <h1>Tasks List</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </div>);
+    }
+}
+
+export default Tasks;
